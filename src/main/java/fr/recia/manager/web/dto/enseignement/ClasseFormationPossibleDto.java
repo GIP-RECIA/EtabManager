924c5ebe6c9c27f3b4b1b55b4aa20cfbdf14e289
cailbourdin <nathan.cailbourdin@recia.fr>
2026-06-19 17:03:10 +0200

    feat(WIP): récupération de tous les formations d'un établissement

--- src/main/java/fr/recia/manager/web/dto/enseignement/ClasseFormationPossibleDto.java
+++ src/main/java/fr/recia/manager/web/dto/enseignement/ClasseFormationPossibleDto.java
@@ -1,9 +1,27 @@
 package fr.recia.manager.web.dto.enseignement;
 
+import fr.recia.manager.db.dto.groupe.ClasseDto;
+import fr.recia.manager.db.entities.education.Enseignement;
+import fr.recia.manager.db.entities.groupe.Classe;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class ClasseFormationPossibleDto {
     private long id;
     private String libelle;
+    private List<EnseignementFormationPossibleDto> enseignements;
+
+    public ClasseFormationPossibleDto(ClasseDto classe){
+        this.id = classe.getId();
+        this.libelle = classe.getCn();
+        this.enseignements = new ArrayList<>();
+    }
+
+    public void addEnseignement(Enseignement enseignement){
+        EnseignementFormationPossibleDto enseignementFormationPossibleDto = new EnseignementFormationPossibleDto(enseignement);
+        this.enseignements.add(enseignementFormationPossibleDto);
+    }
 }
