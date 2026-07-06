1ee24153e32f0b81efdd6ccc2d68e16256c6573a
cailbourdin <nathan.cailbourdin@recia.fr>
2026-07-06 17:14:24 +0200

    feat: routes pour informer des permissions du principal

--- src/main/java/fr/recia/manager/web/rest/ConfigurationController.java
+++ src/main/java/fr/recia/manager/web/rest/ConfigurationController.java
@@ -17,15 +17,23 @@
 package fr.recia.manager.web.rest;
 
 import fr.recia.manager.configuration.AppProperties;
+import fr.recia.manager.db.entities.structure.AStructure;
+import fr.recia.manager.security.AppRole;
+import fr.recia.manager.security.AppUser;
+import fr.recia.manager.services.db.StructureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -36,6 +44,9 @@ public class ConfigurationController {
     @Autowired
     private AppProperties appProperties;
 
+    @Autowired
+    private StructureService structureService;
+
     @GetMapping()
     public ResponseEntity<Object> getConfiguration() {
         Map<String, Object> data = new HashMap<>();
@@ -43,4 +54,28 @@ public class ConfigurationController {
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
+    @GetMapping("/principal")
+    public ResponseEntity<List<AppRole>> getPrincipalRights(@AuthenticationPrincipal AppUser principal) {
+        List<AppRole> rolesForFront = new ArrayList<>();
+        for(AppRole appRole : principal.getRightsForEtabs().keySet()){
+            if(!principal.getRightsForEtabs().get(appRole).isEmpty()){
+                rolesForFront.add(appRole);
+            }
+        }
+        rolesForFront.addAll(principal.getGlobalRights());
+        return new ResponseEntity<>(rolesForFront, HttpStatus.OK);
+    }
+
+    @GetMapping("/principal/{id}")
+    public ResponseEntity<List<AppRole>> getPrincipalRightsForEtab(@AuthenticationPrincipal AppUser principal, @PathVariable Long id) {
+        AStructure structure = structureService.getStructureDBFromId(id);
+        List<AppRole> rolesForFront = new ArrayList<>();
+        for(AppRole appRole : principal.getRightsForEtabs().keySet()){
+            if(principal.getRightsForEtabs().get(appRole).contains(structure.getSiren())){
+                rolesForFront.add(appRole);
+            }
+        }
+        rolesForFront.addAll(principal.getGlobalRights());
+        return new ResponseEntity<>(rolesForFront, HttpStatus.OK);
+    }
 }
